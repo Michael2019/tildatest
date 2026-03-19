fdc9a1025847509117f4d1c7beec6fb15e75c7d7
Michael2019 <80174430+Michael2019@users.noreply.github.com>
2026-03-19 11:59:59 +0300

    config.py

--- config.py
+++ config.py
@@ -2,19 +2,13 @@ import os
 from datetime import timedelta
 
 class Config:
-    # Секретный ключ для JWT (обязательно поменяй на свой!)
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-    
-    # Время жизни токена (например, 7 дней)
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
     
-    # Настройки Google Sheets
+    # Ссылка на лист с шаблонами постов
     SHEETS_CSV_URL = os.environ.get("SHEETS_CSV_URL")
     
-    # ID листа с пользователями (должен называться 'Users')
-    USERS_SHEET_NAME = "Users"
-    
-    # ID листа с шаблонами постов
-    TEMPLATES_SHEET_NAME = "Templates"
+    # Ссылка на лист с пользователями
+    USERS_CSV_URL = os.environ.get("USERS_CSV_URL")
 
 config = Config()
