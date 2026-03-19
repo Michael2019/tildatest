470c6621534d88b3cc18aab787dce23be0f7cf73
Michael2019 <80174430+Michael2019@users.noreply.github.com>
2026-03-19 12:48:41 +0300

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import csv
 from io import StringIO
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 
 import config
 import auth
